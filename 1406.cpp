d5e0cf3204a4e823a7fb4b17b660246e48bec4f6
jsh15932 <jsh1400512@naver.com>
2020-04-07 14:45:10 +0900

    Create 1406.cpp

---
+++ 1406.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	string s;
+	
+	cin >> s;
+	cin >> n;
+	
+	stack<char> s1, s2;
+	
+    for(auto &x:s){
+    	s1.push(x);
+	}
+	
+	while(n--) {
+		char c;
+		cin >> c;
+		
+		if(c == 'L') {
+			if(!s1.empty()) {
+				s2.push(s1.top());
+				s1.pop();
+			}
+		}
+		
+		else if(c == 'D') {
+			if(!s2.empty()) {
+				s1.push(s2.top());
+				s2.pop();
+			}
+		}
+		
+		else if(c == 'B') {
+			if(!s1.empty()) {
+				s1.pop();
+			}
+		}
+		
+		else if(c == 'P') {
+			char x;
+			cin >> x;
+			s1.push(x);
+		}
+	}
+	
+	while(!s1.empty()) {
+		s2.push(s1.top());
+		s1.pop();
+	}
+	
+	while(!s2.empty()) {
+		cout << s2.top();
+		s2.pop();
+	}
+}
