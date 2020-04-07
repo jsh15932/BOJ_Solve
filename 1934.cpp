0148fbc70fa2bbaf63dbb2e3689889dcdf043c8f
jsh15932 <jsh1400512@naver.com>
2020-04-07 18:08:29 +0900

    Create 1934.cpp

---
+++ 1934.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	if(b == 0) {
+		return a;
+	}
+	else {
+		return gcd(b, a % b);
+	}
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+int main() {
+	int n, a, b;
+	cin >> n;
+
+	while(n--) {
+		cin >> a >> b;
+		cout << lcm(a, b) << '\n';
+	}
+}
