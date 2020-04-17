32c52a27ebebf63f3141bbbf384e72d21e749ee2
jsh15932 <jsh1400512@naver.com>
2020-04-18 04:20:07 +0900

    Update 4963.cpp

--- 4963.cpp
+++ 4963.cpp
@@ -4,19 +4,17 @@ using namespace std;
 
 int dx[] = {-1, -1, -1, 1, 1, 1, 0, 0};
 int dy[] = {-1, 0, 1, -1, 0, 1, -1, 1};
-int d[51][51];
+int map[51][51];
 int n, m;
 
 void dfs(int x, int y) {
-	d[x][y] = 0;
+	map[x][y] = 0;
 	
 	for(int i = 0; i < 8; i++) {
-		if(x + dx[i] < 0 || y + dy[i] < 0 || x + dx[i] >= n || y + dy[i] >= m) {
-			continue;
-		}
-		
-		if(d[x + dx[i]][y + dy[i]] == 1) {
-			dfs(x + dx[i], y + dy[i]);
+		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < m) {
+			if(map[x + dx[i]][y + dy[i]] == 1) {
+				dfs(x + dx[i], y + dy[i]);
+			}
 		}
 	}
 }
@@ -33,19 +31,19 @@ int main() {
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
-				cin >> d[i][j];
+				cin >> map[i][j];
 			}
 		}
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
-				if(d[i][j] == 1) {
+				if(map[i][j] == 1) {
 					dfs(i, j);
 					cnt++;
 				}
 			}
 		}
 		
-		cout << cnt << '\n';
+		cout << cnt << endl;
 	}
 }
