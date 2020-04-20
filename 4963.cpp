786b222cebe52e92e142c62243400a0baf99411c
jsh15932 <jsh1400512@naver.com>
2020-04-21 02:42:11 +0900

    Update 4963.cpp

--- 4963.cpp
+++ 4963.cpp
@@ -6,13 +6,14 @@ int dx[] = {-1, -1, -1, 1, 1, 1, 0, 0};
 int dy[] = {-1, 0, 1, -1, 0, 1, -1, 1};
 int map[51][51];
 int n, m;
+int cnt;
 
 void dfs(int x, int y) {
 	map[x][y] = 0;
 	
 	for(int i = 0; i < 8; i++) {
 		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < m) {
-			if(map[x + dx[i]][y + dy[i]] == 1) {
+			if(map[x + dx[i]][y + dy[i]] == 1){
 				dfs(x + dx[i], y + dy[i]);
 			}
 		}
@@ -23,11 +24,11 @@ int main() {
 	while(1) {
 		cin >> m >> n;
 		
-		int cnt = 0;
-		
-		if(n == 0 && m == 0) {
+		if(n == 0 & m == 0) {
 			break;
 		}
+		
+		cnt = 0;
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
