7e38b12c7ce363170ac012412188288c8fdfbc93
jsh15932 <jsh1400512@naver.com>
2020-04-22 06:16:02 +0900

    Update 11724.cpp

--- 11724.cpp
+++ 11724.cpp
@@ -4,32 +4,30 @@
 using namespace std;
 
 bool chk[1001];
-vector <int> v[1001];
+vector <int> vc[1001];
 
 void dfs(int node) {
 	chk[node] = true;
 	
-	for(int i = 0; i < v[node].size(); i++) {
-		int next = v[node][i];
-		
-		if(!chk[next]) {
-			dfs(next);
+	for(int i = 0; i < vc[node].size(); i++) {
+		if(!chk[vc[node][i]]) {
+			dfs(vc[node][i]);
 		}
 	}
 }
 
 int main() {
 	int n, m;
-	int a, b;
+	int v1, v2;
 	int cnt = 0;
 	
 	cin >> n >> m;
 	
 	for(int i = 0; i < m; i++) {
-		cin >> a >> b;
+		cin >> v1 >> v2;
 		
-		v[a].push_back(b);
-		v[b].push_back(a);
+		vc[v1].push_back(v2);
+		vc[v2].push_back(v1);
 	}
 	
 	for(int i = 1; i <= n; i++) {
@@ -37,7 +35,7 @@ int main() {
 			dfs(i);
 			cnt++;
 		}
-	}
+	} 
 	
 	cout << cnt;
 }
