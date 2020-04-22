92c1b6fa4cb14545b3460aa5ffd027d31e1bdded
jsh15932 <jsh1400512@naver.com>
2020-04-23 05:07:56 +0900

    Create 10815.cpp

---
+++ 10815.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m, num;
+vector <int> vc;
+
+int main() {
+	scanf("%d", &n);
+	
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &num);
+		vc.push_back(num);
+	}
+	
+	sort(vc.begin(), vc.end());
+	
+	scanf("%d", &m);
+	
+	for(int i = 0; i < m; i++) {
+		scanf("%d", &num);
+		
+		if(binary_search(vc.begin(), vc.end(), num)) {
+			printf("1 ");
+		}
+		
+		else {
+			printf("0 ");
+		}
+	}
+}
