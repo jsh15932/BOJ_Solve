3fd4ef0d41dc26f402da81b0555575a57892f91a
jsh15932 <jsh1400512@naver.com>
2020-04-28 18:09:30 +0900

    Create 1783.cpp

---
+++ 1783.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+
+int main() {
+	cin >> n >> m;
+	
+	if(n == 1) {
+		cout << 1;
+	}
+	
+	else if(n == 2) {
+		cout << min(4, (m + 1) / 2);
+	}
+	
+	else if(n >= 3) {
+		if(m <= 6) {
+			cout << min(4, m);
+		}
+		
+		else {
+			cout << m - 2;
+		}
+	}
+}
