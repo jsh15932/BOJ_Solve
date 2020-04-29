6f91545953c28847c9a451a00e62495557606ca6
jsh15932 <jsh1400512@naver.com>
2020-04-29 14:51:39 +0900

    Update 4963.cpp

--- 4963.cpp
+++ 4963.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int dx[] = {-1, -1, -1, 1, 1, 1, 0, 0};
 int dy[] = {-1, 0, 1, -1, 0, 1, -1, 1};
 int map[51][51];
-int n, m;
+int w, h;
 int cnt;
 
 void dfs(int x, int y) {
 	map[x][y] = 0;
 	
 	for(int i = 0; i < 8; i++) {
-		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < m) {
+		for(int j = 0; j < 8; j++) {
 			if(map[x + dx[i]][y + dy[i]] == 1) {
 				dfs(x + dx[i], y + dy[i]);
 			}
@@ -22,22 +22,22 @@ void dfs(int x, int y) {
 
 int main() {
 	while(1) {
-		cin >> m >> n;
-		
-		if(n == 0 && m == 0) {
-			break;
-		}
+		cin >> h >> w;
 		
 		cnt = 0;
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				cin >> map[i][j];
+		if(w == 0 && h == 0) {
+			break;
+		}
+		
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
+				scanf("%1d", &map[i][j]);
 			}
 		}
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
 				if(map[i][j] == 1) {
 					dfs(i, j);
 					cnt++;
