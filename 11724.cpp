fbf4cc83696ee62dc8eb7196ea2f5d51884bcfda
jsh15932 <jsh1400512@naver.com>
2020-05-05 17:12:46 +0900

    Update 11724.cpp

--- 11724.cpp
+++ 11724.cpp
@@ -3,27 +3,27 @@
 #include<algorithm>
 using namespace std;
 
-bool chk[1001];
+int n, m;
+bool visited[1001];
 vector <int> vc[1001];
+int cnt;
 
 void dfs(int node) {
-	chk[node] = true;
+	visited[node] = true;
 	
 	for(int i = 0; i < vc[node].size(); i++) {
-		if(!chk[vc[node][i]]) {
+		if(!visited[vc[node][i]]) {
 			dfs(vc[node][i]);
 		}
 	}
 }
 
 int main() {
-	int n, m;
-	int v1, v2;
-	int cnt = 0;
-	
 	cin >> n >> m;
 	
-	for(int i = 0; i < m; i++) {
+	for(int i = 1; i <= m; i++) {
+		int v1, v2;
+		
 		cin >> v1 >> v2;
 		
 		vc[v1].push_back(v2);
@@ -31,11 +31,11 @@ int main() {
 	}
 	
 	for(int i = 1; i <= n; i++) {
-		if(!chk[i]) {
+		if(!visited[i]) {
 			dfs(i);
 			cnt++;
 		}
-	} 
+	}
 	
 	cout << cnt;
 }
