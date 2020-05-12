a0428ad50a9685a948225e4b972371779a493205
jsh15932 <jsh1400512@naver.com>
2020-05-13 01:55:14 +0900

    Update 11376.cpp

--- 11376.cpp
+++ 11376.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int n, m;
 int a, b;
+int ans;
 int d[1001];
-bool visited[1001];
 vector < vector <int> > vc;
+bool visited[1001];
 
 bool dfs(int cur) {
 	if(visited[cur]) {
@@ -22,27 +23,12 @@ bool dfs(int cur) {
 		
 		if(!d[next] || dfs(d[next])) {
 			d[next] = cur;
+			
 			return true;
 		}
 	}
 	
 	return false;
-}
-
-int bipartite_match() {
-	int res = 0;
-	
-	for(int i = 1; i <= n; i++) {
-		for(int j = 0; j < 2; j++) {
-			memset(visited, false, sizeof(visited));
-			
-			if(dfs(i)) {
-				res++;
-			}
-		}
-	}
-	
-	return res;
 }
 
 int main() {
@@ -59,5 +45,15 @@ int main() {
 		}
 	}
 	
-	cout << bipartite_match();
+	for(int i = 1; i <= n; i++) {
+		for(int j = 0; j < 2; j++) {
+			memset(visited, false, sizeof(visited));
+			
+			if(dfs(i)) {
+				ans++;
+			}
+		}
+	}
+	
+	cout << ans;
 }
