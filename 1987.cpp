50b16774ea77d9beae187993b602f768d34c0d52
jsh15932 <jsh1400512@naver.com>
2020-05-15 04:59:42 +0900

    Update 1987.cpp

--- 1987.cpp
+++ 1987.cpp
@@ -1,38 +1,39 @@
 #include<iostream>
-#include<vector>
 #include<cstring>
 #include<algorithm>
 using namespace std;
 
-int r, c;
+int n, m;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
 int map[21][21];
-bool check[27];
+bool chk[27];
 string s;
 
 int dfs(int x, int y, int k) {
 	int res = 0;
-	check[map[x][y]] = true;
+	chk[map[x][y]] = true;
 	
 	for(int i = 0; i < 4; i++) {
-		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < r && y + dy[i] < c && !check[map[x + dx[i]][y + dy[i]]]) {
-			res = max(res, dfs(x + dx[i], y + dy[i], k + 1));
+		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < m) {
+			if(!chk[map[x + dx[i]][y + dy[i]]]) {
+				res = max(res, dfs(x + dx[i], y + dy[i], k + 1));
+			}
 		}
 	}
 	
-	check[map[x][y]] = false;
+	chk[map[x][y]] = false;
 	
 	return res + 1;
 }
 
 int main() {
-	cin >> r >> c;
+	cin >> n >> m;
 	
-	for(int i = 0; i < r; i++) {
+	for(int i = 0; i < n; i++) {
 		cin >> s;
 		
-		for(int j = 0; j < c; j++) {
+		for(int j = 0; j < m; j++) {
 			map[i][j] = s[j] - 'A';
 		}
 	}
