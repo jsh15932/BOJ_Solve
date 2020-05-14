da4910739c79070aa342d14b79005588dfb1626c
jsh15932 <jsh1400512@naver.com>
2020-05-15 04:37:43 +0900

    Update 1987.cpp

--- 1987.cpp
+++ 1987.cpp
@@ -8,20 +8,20 @@ int r, c;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
 int map[21][21];
-int check[27];
+bool check[27];
 string s;
 
 int dfs(int x, int y, int k) {
 	int res = 0;
-	check[map[x][y]] = 1;
+	check[map[x][y]] = true;
 	
 	for(int i = 0; i < 4; i++) {
-		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < r && y + dy[i] < c && check[map[x + dx[i]][y + dy[i]]] == 0) {
+		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < r && y + dy[i] < c && !check[map[x + dx[i]][y + dy[i]]]) {
 			res = max(res, dfs(x + dx[i], y + dy[i], k + 1));
 		}
 	}
 	
-	check[map[x][y]] = 0;
+	check[map[x][y]] = false;
 	
 	return res + 1;
 }
