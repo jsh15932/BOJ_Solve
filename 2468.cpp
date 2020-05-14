e48b5508ba69df925839acbf761571a66de359b6
jsh15932 <jsh1400512@naver.com>
2020-05-15 03:22:58 +0900

    Update 2468.cpp

--- 2468.cpp
+++ 2468.cpp
@@ -3,18 +3,17 @@
 #include<algorithm>
 using namespace std;
 
-int n, res, ans, safe;
+int n, res, ans, cnt;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
 int map[101][101];
 bool visited[101][101];
 
 void dfs(int x, int y, int h) {
-	visited[x][y] = true;
-	
 	for(int i = 0; i < 4; i++) {
 		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < n) {
 			if(map[x + dx[i]][y + dy[i]] > h && !visited[x + dx[i]][y + dy[i]]) {
+				visited[x + dx[i]][y + dy[i]] = true;
 				dfs(x + dx[i], y + dy[i], h);
 			}
 		}
@@ -33,21 +32,21 @@ int main() {
 	}
 	
 	for(int h = 0; h <= res; h++) {
-		safe = 0;
+		cnt = 0;
 		
 		memset(visited, false, sizeof(visited));
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
 				if(map[i][j] > h && !visited[i][j]) {
+					visited[i][j] = true;
 					dfs(i, j, h);
-					
-					safe++;
+					cnt++;
 				}
 			}
 		}
 		
-		ans = max(ans, safe);
+		ans = max(ans, cnt);
 	}
 	
 	cout << ans;
