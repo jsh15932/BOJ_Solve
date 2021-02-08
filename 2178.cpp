bbe1af36510f74b15309ff6e3d44ba4e756c1a48
jsh15932 <jsh1400512@naver.com>
2021-02-08 11:27:03 +0900

    Update 2178.cpp

--- 2178.cpp
+++ 2178.cpp
@@ -1,15 +1,13 @@
-#include<iostream>
-#include<queue>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 int n, m;
-int map[101][101];
+int dp[101][101];
 int cnt[101][101];
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
 bool visited[101][101];
-queue < pair <int, int> > que;
+queue < pair < int, int > > que;
 
 void bfs() {
 	while(!que.empty()) {
@@ -20,9 +18,9 @@ void bfs() {
 		
 		for(int i = 0; i < 4; i++) {
 			if(q1 + dx[i] >= 0 && q2 + dy[i] >= 0 && q1 + dx[i] < n && q2 + dy[i] < m) {
-				if(map[q1 + dx[i]][q2 + dy[i]] == 1 && !visited[q1 + dx[i]][q2 + dy[i]]) {
-					que.push(make_pair(q1 + dx[i], q2 + dy[i]));
+				if(dp[q1 + dx[i]][q2 + dy[i]] == 1 && !visited[q1 + dx[i]][q2 + dy[i]]) {
 					visited[q1 + dx[i]][q2 + dy[i]] = true;
+					que.push(make_pair(q1 + dx[i], q2 + dy[i]));
 					cnt[q1 + dx[i]][q2 + dy[i]] = cnt[q1][q2] + 1;
 				}
 			}
@@ -35,16 +33,15 @@ int main() {
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < m; j++) {
-			scanf("%1d", &map[i][j]);
-			visited[i][j] = false;
+			scanf("%1d", &dp[i][j]);
 		}
 	}
 	
 	que.push(make_pair(0, 0));
-	visited[0][0]= true;
+	visited[0][0] = true;
 	cnt[0][0] = 1;
 	
 	bfs();
 	
-	cout << cnt[n - 1][m - 1];
+	printf("%d", cnt[n - 1][m - 1]);
 }
