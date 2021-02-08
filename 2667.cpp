a6d3effa902734b34eb1dc372723884df0a720f2
jsh15932 <jsh1400512@naver.com>
2021-02-08 12:37:39 +0900

    Update 2667.cpp

--- 2667.cpp
+++ 2667.cpp
@@ -1,39 +1,39 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int n, cnt;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
-int dp[26][26];
+int map[26][26];
+int cnt, n;
 vector <int> vc;
 
 void dfs(int x, int y) {
-	dp[x][y] = 0;
+	map[x][y] = 0;
 	
 	for(int i = 0; i < 4; i++) {
-		for(int j = 0; j < 4; j++) {
-			if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < n) {
-				if(dp[x + dx[i]][y + dy[i]] == 1) {
-					cnt++;
-					dfs(x + dx[i], y + dy[i]);
-				}
-			}
+		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < n) {
+		    if(map[x + dx[i]][y + dy[i]] == 1) {
+			dfs(x + dx[i], y + dy[i]);
+			cnt++;
+		    }
 		}
 	}
 }
 
 int main() {
-	scanf("%d", &n);
+	cin >> n;
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			scanf("%1d", &dp[i][j]);
+			scanf("%1d", &map[i][j]);
 		}
 	}
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			if(dp[i][j] == 1) {
+			if(map[i][j] == 1) {
 				cnt = 1;
 				dfs(i, j);
 				vc.push_back(cnt);
@@ -41,11 +41,11 @@ int main() {
 		}
 	}
 	
-	printf("%d\n", vc.size());
+	cout << vc.size() << '\n';
 	
 	sort(vc.begin(), vc.end());
 	
 	for(int i = 0; i < vc.size(); i++) {
-		printf("%d\n", vc[i]);
+		cout << vc[i] << endl;
 	}
 }
