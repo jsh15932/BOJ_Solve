5050899ec05a7e3e290786b86eab4740e1f82ece
jsh15932 <jsh1400512@naver.com>
2021-02-08 18:58:13 +0900

    Update 7569.cpp

--- 7569.cpp
+++ 7569.cpp
@@ -1,10 +1,8 @@
-#include<iostream>
-#include<queue>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, m, h, ans;
-int map[101][101][101];
+int n, m, h;
+int dp[101][101][101];
 int dx[] = {-1, 1, 0, 0, 0, 0};
 int dy[] = {0, 0, -1, 1, 0, 0};
 int dz[] = {0, 0, 0, 0, -1, 1};
@@ -20,27 +18,33 @@ void bfs() {
 		
 		for(int i = 0; i < 6; i++) {
 			if(q1 + dx[i] >= 0 && q2 + dy[i] >= 0 && q3 + dz[i] >= 0 && q1 + dx[i] < h && q2 + dy[i] < n && q3 + dz[i] < m) {
-				if(map[q1 + dx[i]][q2 + dy[i]][q3 + dz[i]] == 0) {
-					map[q1 + dx[i]][q2 + dy[i]][q3 + dz[i]] = map[q1][q2][q3] + 1;
+				if(dp[q1 + dx[i]][q2 + dy[i]][q3 + dz[i]] == 0) {
+					dp[q1 + dx[i]][q2 + dy[i]][q3 + dz[i]] = dp[q1][q2][q3] + 1;
 					que.push(make_pair(make_pair(q1 + dx[i], q2 + dy[i]), q3 + dz[i]));
 				}
-			} 
+			}
 		}
 	}
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
 	cin >> m >> n >> h;
 	
-	for(int i = 0; i < h; i++) {
+	int ans = 0;
+	
+	for(int i = 0; i < h; i ++) {
 		for(int j = 0; j < n; j++) {
 			for(int k = 0; k < m; k++) {
-				cin >> map[i][j][k];
+				cin >> dp[i][j][k];
 				
-				if(map[i][j][k] == 1) {
+				if(dp[i][j][k] == 1) {
 					que.push(make_pair(make_pair(i, j), k));
 				}
-			}
+			}	
 		}
 	}
 	
@@ -49,17 +53,17 @@ int main() {
 	for(int i = 0; i < h; i++) {
 		for(int j = 0; j < n; j++) {
 			for(int k = 0; k < m; k++) {
-				if(map[i][j][k] == 0) {
-					cout << -1;
+				if(dp[i][j][k] == 0) {
+					cout << "-1";
 					return 0;
 				}
 				
-				if(map[i][j][k] > ans) {
-					ans = map[i][j][k];
+				if(dp[i][j][k] > ans) {
+					ans = dp[i][j][k];
 				}
 			}
 		}
 	}
 	
-	cout << ans - 1;
+	cout << ans - 1 << endl;
 }
