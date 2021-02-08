3a57f222e158733295a8c163faa27e1edb8a2676
jsh15932 <jsh1400512@naver.com>
2021-02-08 18:26:04 +0900

    Update 2468.cpp

--- 2468.cpp
+++ 2468.cpp
@@ -1,20 +1,16 @@
-#include<iostream>
-#include<cstring>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, h, cnt;
-int map[101][101];
+int n;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
+int dp[101][101];
 bool visited[101][101];
-int res = 0;
-int ans = 0;
 
 void dfs(int x, int y, int h) {
 	for(int i = 0; i < 4; i++) {
 		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < n) {
-			if(map[x + dx[i]][y + dy[i]] > h && !visited[x + dx[i]][y + dy[i]]) {
+			if(dp[x + dx[i]][y + dy[i]] > h && !visited[x + dx[i]][y + dy[i]]) {
 				visited[x + dx[i]][y + dy[i]] = true;
 				dfs(x + dx[i], y + dy[i], h);
 			}
@@ -23,26 +19,33 @@ void dfs(int x, int y, int h) {
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
 	cin >> n;
+	
+	int res = 0;
+	int ans = 0;
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			cin >> map[i][j];
+			cin >> dp[i][j];
 			
-			if(map[i][j] > res) {
-				res = map[i][j];
+			if(dp[i][j] > res) {
+				res = dp[i][j];
 			}
 		}
 	}
 	
 	for(int i = 0; i <= res; i++) {
-		cnt = 0;
+		int cnt = 0;
 		
 		memset(visited, false, sizeof(visited));
 		
 		for(int j = 0; j < n; j++) {
 			for(int k = 0; k < n; k++) {
-				if(map[j][k] > i && !visited[j][k]) {
+				if(dp[j][k] > i && !visited[j][k]) {
 					visited[j][k] = true;
 					dfs(j, k, i);
 					cnt++;
@@ -55,5 +58,5 @@ int main() {
 		}
 	}
 	
-	cout << ans;
+	cout << ans << endl;
 }
