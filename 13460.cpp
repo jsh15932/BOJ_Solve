bea11da6e734c244dadc74317ff1775911451e2e
jsh15932 <jsh1400512@naver.com>
2021-02-14 13:17:17 +0900

    Update 13460.cpp

--- 13460.cpp
+++ 13460.cpp
@@ -1,15 +1,12 @@
-#include<iostream>
-#include<queue>
-#include<cstring>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 int n, m, res;
 int bx, by, rx, ry;
-int dis[11][11][11][11];
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
-char c[11][11];
+int dp[11][11][11][11];
+char s[11][11];
 bool visited[11][11][11][11];
 queue < pair < pair < int, int >, pair < int, int > > > que;
 
@@ -25,48 +22,48 @@ int bfs() {
 			
 			que.pop();
 			
-			if(c[q1][q2] == 'O' && c[q1][q2] != c[q3][q4]) {
+			if(s[q1][q2] == 'O' && s[q1][q2] != s[q3][q4]) {
 				return res;
 			}
 			
 			for(int i = 0; i < 4; i++) {
-				int c_q1 = q1;
-				int c_q2 = q2;
-				int c_q3 = q3;
-				int c_q4 = q4;
+				int nq1 = q1;
+				int nq2 = q2;
+				int nq3 = q3;
+				int nq4 = q4;
 				
-				while(c[c_q1][c_q2] != 'O' && c[c_q1 + dx[i]][c_q2 + dy[i]] != '#') {
-					c_q1 += dx[i];
-					c_q2 += dy[i];
+				while(s[nq1][nq2] != 'O' && s[nq1 + dx[i]][nq2 + dy[i]] != '#') {
+					nq1 += dx[i];
+					nq2 += dy[i];
 				}
 				
-				while(c[c_q3][c_q4] != 'O' && c[c_q3 + dx[i]][c_q4 + dy[i]] != '#') {
-					c_q3 += dx[i];
-					c_q4 += dy[i];
+				while(s[nq3][nq4] != 'O' && s[nq3 + dx[i]][nq4 + dy[i]] != '#') {
+					nq3 += dx[i];
+					nq4 += dy[i];
 				}
 				
-				if(c_q1 == c_q3 && c_q2 == c_q4) {
-					if(c[c_q1][c_q2] == 'O') {
+				if(nq1 == nq3 && nq2 == nq4) {
+					if(s[nq1][nq2] == 'O') {
 						continue;
 					}
 					
-					if(abs(c_q1 - q1) + abs(c_q2 - q2) < abs(c_q3 - q3) + abs(c_q4 - q4)) {
-						c_q3 -= dx[i];
-						c_q4 -= dy[i];
+					if(abs(nq1 - q1) + abs(nq2 - q2) > abs(nq3 - q3) + abs(nq4 - q4)) {
+						nq1 -= dx[i];
+						nq2 -= dy[i];
 					}
 					
 					else {
-						c_q1 -= dx[i];
-						c_q2 -= dy[i];
+						nq3 -= dx[i];
+						nq4 -= dy[i];
 					}
 				}
 				
-				if(dis[c_q1][c_q2][c_q3][c_q4]) {
+				if(dp[nq1][nq2][nq3][nq4]) {
 					continue;
 				}
 				
-				que.push(make_pair(make_pair(c_q1, c_q2), make_pair(c_q3, c_q4)));
-				dis[c_q1][c_q2][c_q3][c_q4] = 1;
+				que.push(make_pair(make_pair(nq1, nq2), make_pair(nq3, nq4)));
+				dp[nq1][nq2][nq3][nq4] = 1;
 			}
 		}
 		
@@ -81,20 +78,22 @@ int bfs() {
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
 	cin >> n >> m;
 	
 	for(int i = 0; i < n; i++) {
-		getchar();
-		
 		for(int j = 0; j < m; j++) {
-			cin >> c[i][j];
+			cin >> s[i][j];
 			
-			if(c[i][j] == 'B') {
+			if(s[i][j] == 'B') {
 				bx = i;
 				by = j;
 			}
 			
-			else if(c[i][j] == 'R') {
+			else if(s[i][j] == 'R') {
 				rx = i;
 				ry = j;
 			}
@@ -102,7 +101,7 @@ int main() {
 	}
 	
 	que.push(make_pair(make_pair(rx, ry), make_pair(bx, by)));
-	dis[rx][ry][bx][by] = 1;
+	dp[rx][ry][bx][by] = 1;
 	
-	cout << bfs();
+	cout << bfs() << endl;
 }
