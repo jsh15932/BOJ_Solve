6d4ae942f695febb98cb0cb3976196e8b2cbbcba
SeungHoon Jang <jsh1400512@naver.com>
2021-08-14 14:44:14 +0900

    Commit

---
+++ 1922.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<pair<int, int>> vc[1001];
+bool visited[1001];
+
+int main() {
+	cin >> n >> m;
+	
+	int res = 0;
+	
+	for(int i = 0; i < m; i++) {
+		int a, b, c;
+		
+		cin >> a >> b >> c;
+		vc[a].push_back(make_pair(b, c));
+		vc[b].push_back(make_pair(a, c));
+	}
+	
+	priority_queue<pair<int, int>> pq;
+	pq.push(make_pair(0, 1));
+	
+	while(!pq.empty()) {
+		int cur_cost = -pq.top().first;
+		int cur_idx = pq.top().second;
+		
+		pq.pop();
+		
+		if(visited[cur_idx]) {
+			continue;
+		}
+		
+		visited[cur_idx] = true;
+		res += cur_cost;
+		
+		for(auto c : vc[cur_idx]) {
+			int next_idx = c.first;
+			int next_cost = c.second;
+			
+			if(visited[next_idx]) {
+				continue;
+			}
+			
+			pq.push(make_pair(-next_cost, next_idx));
+		}
+	}
+	
+	cout << res << endl;
+}
