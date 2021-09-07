179f7d340c5b07da4632bfdacfb64232a2987ef6
jsh15932 <jsh1400512@naver.com>
2021-09-08 01:08:56 +0900

    Commit

--- 11404.cpp
+++ 11404.cpp
@@ -1,47 +1,43 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 int n, m;
-int from, to, cost;
-int map[101][101];
-
-void floyd() {
-	for(int via = 1; via <= n; via++) {
-		for(int from = 1; from <= n; from++) {
-			for(int to = 1; to <= n; to++) {
-				if(map[from][via] == 0 || map[via][to] == 0 || from == to) {
-					continue;
-				}
-				
-				if(map[from][to] == 0 || map[from][via] + map[via][to] < map[from][to]) {
-					map[from][to] = map[from][via] + map[via][to];
-				}
-			}
-		}
-	}
-}
+int arr[101][101];
 
 int main() {
 	cin >> n >> m;
 	
 	for(int i = 0; i < m; i++) {
-		cin >> from >> to >> cost;
+		int a, b, c;
 		
-		if(map[from][to]) {
-			map[from][to] = min(map[from][to], cost);
+		cin >> a >> b >> c;
+		
+		if(arr[a][b]) {
+			arr[a][b] = min(c, arr[a][b]);
 		}
 		
 		else {
-			map[from][to] = cost;
+			arr[a][b] = c;
 		}
 	}
 	
-	floyd();
+	for(int via = 1; via <= n; via++) {
+		for(int from = 1; from <= n; from++) {
+			for(int to = 1; to <= n; to++) {
+				if(from == to || arr[from][via] == 0 || arr[via][to] == 0) {
+					continue;
+				}
+				
+				if(arr[from][to] == 0 || arr[from][via] + arr[via][to] < arr[from][to]) {
+					arr[from][to] = arr[from][via] + arr[via][to];
+				}
+			}
+		}
+	}
 	
 	for(int i = 1; i <= n; i++) {
 		for(int j = 1; j <= n; j++) {
-			cout << map[i][j] << ' ';
+			cout << arr[i][j] << ' ';
 		}
 		
 		cout << endl;
