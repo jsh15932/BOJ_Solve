416599306b7a82ad712a0b5ef1c2589957e49d06
jsh15932 <jsh1400512@naver.com>
2021-09-07 18:10:40 +0900

    Commit

--- 1753.cpp
+++ 1753.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define INF 987654321
 
 int v, e, k;
-int dp[200001];
-priority_queue < pair < int, int > > pq;
-vector < pair < int, int > > vc[200001];
+int arr[20001];
+priority_queue< pair<int, int> > pq;
+vector< pair <int, int> > vc[20001];
 
 void dijkstra() {
 	while(!pq.empty()) {
@@ -18,41 +18,45 @@ void dijkstra() {
 			int next = vc[cur][i].first;
 			int next_cost = vc[cur][i].second;
 			
-			if(dp[next] > cost + next_cost) {
-				dp[next] = cost + next_cost;
-				pq.push(make_pair(-dp[next], next));
+			if(arr[next] > cost + next_cost) {
+				arr[next] = cost + next_cost;
+				pq.push({ -arr[next], next });
 			}
 		}
 	}
 }
 
 int main() {
-	scanf("%d %d %d", &v, &e, &k);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> v >> e >> k;
 	
 	for(int i = 0; i < e; i++) {
 		int a, b, c;
 		
-		scanf("%d %d %d", &a, &b, &c);
+		cin >> a >> b >> c;
 		
-		vc[a].push_back(make_pair(b, c));
+		vc[a].push_back({ b, c });
 	}
 	
 	for(int i = 1; i <= v; i++) {
-		dp[i] = INF;
+		arr[i] = INF;
 	}
 	
-	pq.push(make_pair(0, k));
-	dp[k] = 0;
+	pq.push({ 0, k });
+	arr[k] = 0;
 	
 	dijkstra();
 	
 	for(int i = 1; i <= v; i++) {
-		if(dp[i] == INF) {
-			printf("INF\n");
+		if(arr[i] == INF) {
+			cout << "INF" << endl;
 		}
 		
 		else {
-			printf("%d\n", dp[i]);
+			cout << arr[i] << endl;
 		}
 	}
 }
