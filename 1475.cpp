3293bc55e84b940100e0cc07f278dfe40e16a02d
jsh15932 <jsh1400512@naver.com>
2021-12-19 19:08:28 +0900

    Commit

--- 1475.cpp
+++ 1475.cpp
@@ -1,30 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string s;
+vector<int> vc(10);
+
 int main() {
-	string s;
-	vector<int> vc(10);
-	int max = 0, chk = 0;
-	
 	cin >> s;
+	
+	int cnt = 0;
 	
 	for(int i = 0; i < s.size(); i++) {
 		vc[s.at(i) - '0']++;
 		
 		if(s.at(i) - '0' == 6 || s.at(i) - '0' == 9) {
-			chk++;
+			cnt++;
 		}
 	}
+	
+	int max = 0;
 	
 	for(int i = 0; i < 10; i++) {
 		if(i != 6 && i != 9 && vc[i] > max) {
 			max = vc[i];
-		}	
+		}
 	}
 	
-	if((chk + 1) / 2 > max) {
-		max = (chk + 1) / 2;
+	if((cnt + 1) / 2 > max) {
+		max = (cnt + 1) / 2;
 	}
 	
-	cout << max << endl;
+	cout << max;
 }
