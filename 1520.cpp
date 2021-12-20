a0ba6ef2a1eb531c5e96e1bbadda184250c14c46
jsh15932 <jsh1400512@naver.com>
2021-12-21 06:33:06 +0900

    Update 1520.cpp

--- 1520.cpp
+++ 1520.cpp
@@ -1,43 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, m;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
-int n, m;
 int arr[501][501];
 int dp[501][501];
 
 int solve(int x, int y) {
+	if(dp[x][y] != -1) {
+		return dp[x][y];
+	}
+	
 	if(x == 0 && y == 0) {
 		return 1;
 	}
 	
-	int &res = dp[x][y];
-	
-	if(res != -1) {
-		return res;
-	}
-	
-	res = 0;
+	dp[x][y] = 0;
 	
 	for(int i = 0; i < 4; i++) {
 		if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < m) {
 			if(arr[x][y] < arr[x + dx[i]][y + dy[i]]) {
-				res += solve(x + dx[i], y + dy[i]);
-			}
+				dp[x][y] += solve(x + dx[i], y + dy[i]);
+			}	
 		}
 	}
 	
-	return res;
+	return dp[x][y];
 }
 
 int main() {
-	memset(dp, -1, sizeof(dp));
 	cin >> n >> m;
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < m; j++) {
 			cin >> arr[i][j];
+			
+			dp[i][j] = -1;
 		}
 	}
 	
