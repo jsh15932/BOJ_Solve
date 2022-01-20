93951fe368be538af1c5507ddaae3d899576a286
jsh15932 <jsh1400512@naver.com>
2022-01-20 23:16:30 +0900

    Update 1158.cpp

--- 1158.cpp
+++ 1158.cpp
@@ -1,28 +1,27 @@
-#include<iostream>
-#include<queue>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
+int n, k;
+queue<int> que;
+
 int main() {
-	queue <int> q;
-	int n, k;
 	cin >> n >> k;
 	
 	for(int i = 1; i <= n; i++) {
-		q.push(i);
+		que.push(i);
 	}
 	
 	cout << "<";
 	
 	for(int i = 0; i < n - 1; i++) {
 		for(int j = 0; j < k - 1; j++) {
-			q.push(q.front());
-			q.pop();
+			que.push(que.front());
+			que.pop();
 		}
 		
-		cout << q.front() << "," << ' ';
-		q.pop();
+		cout << que.front() << "," << ' ';
+		que.pop();
 	}
 	
-	cout << q.front() << ">";
+	cout << que.front() << ">";
 }
