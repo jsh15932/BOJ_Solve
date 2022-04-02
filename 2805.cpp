a1efaf2d30953a61f46724b81957a05ffa8de531
jsh15932 <jsh15932@jsh15932ui-MacBookPro.local>
2022-04-02 16:25:39 +0900

    Commit

--- 2805.cpp
+++ 2805.cpp
@@ -4,46 +4,42 @@ using namespace std;
 long long int n, m;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	
-	cin >> n >> m;
-	
-	long long int d[n];
-	long long int INF = 0;
-	
-	for(int i = 0; i < n; i++) {
-		cin >> d[i];
-		
-		if(d[i] > INF) {
-			INF = d[i];
-		}
-	}
-	
-	long long int left = 0;
-	long long int right = INF;
-	long long int res = 0;
-	
-	while(left <= right) {
-		long long int mid = (left + right) / 2;
-		long long int total = 0;
-		
-		for(int i = 0; i < n; i++) {
-			if(d[i] > mid) {
-				total += d[i] - mid;
-			}
-		}
-		
-		if(total >= m) {
-			res = mid;
-			left = mid + 1;
-		}
-		
-		else {
-			right = mid - 1;
-		}
-	}
-	
-	cout << res << endl;
+    scanf("%lld %lld", &n, &m);
+    
+    long long int maxN = 0;
+    long long int tree[n + 1];
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%lld", &tree[i]);
+        
+        if(tree[i] > maxN) {
+            maxN = tree[i];
+        }
+    }
+    
+    long long int left = 0;
+    long long int right = maxN;
+    long long int ans = 0;
+    
+    while(left <= right) {
+        long long int mid = (left + right) / 2;
+        long long int total = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(tree[i] > mid) {
+                total += tree[i] - mid;
+            }
+        }
+        
+        if(total >= m) {
+            ans = mid;
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    
+    printf("%lld", ans);
 }
