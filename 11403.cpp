988750844b705a8a44c18e65d4105281d973642e
jsh15932 <jsh15932@jsh15932ui-MacBookPro.local>
2022-04-23 00:59:20 +0900

    Commit

--- 11403.cpp
+++ 11403.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 int n;
-int dp[101][101];
+int arr[101][101];
 
 void floyd() {
-	for(int via = 0; via < n; via++) {
-		for(int from = 0; from < n; from++) {
-			for(int to = 0; to < n; to++) {
-				if(dp[from][via] == 1 && dp[via][to] == 1) {
-					dp[from][to] = 1;
-				}
-			}
-		}
-	}
+    for(int via = 0; via < n; via++) {
+        for(int from = 0; from < n; from++) {
+            for(int to = 0; to < n; to++) {
+                if(arr[from][via] == 1 && arr[via][to] == 1) {
+                    arr[from][to] = 1;
+                }
+            }
+        }
+    } 
 }
 
 int main() {
-	scanf("%d", &n);
-	
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			scanf("%d", &dp[i][j]);
-		}
-	}
-	
-	floyd();
-	
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			printf("%d ", dp[i][j]);
-		}
-		
-		printf("\n");
-	}
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%1d", &arr[i][j]);
+        }
+    }
+    
+    floyd();
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        
+        cout << endl;
+    }
 }
