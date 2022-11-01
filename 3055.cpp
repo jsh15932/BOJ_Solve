a09dca8cd4746ed4198758a633da131a06550e6d
jsh15932 <jsh1400512@naver.com>
2022-11-01 23:17:03 +0900

    Commit

--- 3055.cpp
+++ 3055.cpp
@@ -5,7 +5,7 @@ int n, m;
 int x, y;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, -1, 0, 1};
-char map[51][51];
+char arr[51][51];
 int d[51][51];
 queue < pair < pair < int, int >, int > > que;
 
@@ -22,8 +22,8 @@ void bfs() {
 		
 		for(int i = 0; i < 4; i++) {
 			if(q1 + dx[i] >= 0 && q2 + dy[i] >= 0 && q1 + dx[i] < n && q2 + dy[i] < m) {
-				if(map[q1 + dx[i]][q2 + dy[i]] != 'X' && d[q1 + dx[i]][q2 + dy[i]] == 0) {
-					if(map[q1 + dx[i]][q2 + dy[i]] == 'D') {
+				if(arr[q1 + dx[i]][q2 + dy[i]] != 'X' && d[q1 + dx[i]][q2 + dy[i]] == 0) {
+					if(arr[q1 + dx[i]][q2 + dy[i]] == 'D') {
 					    if(q3 == 0) {
 						    continue;
 					    }
@@ -47,14 +47,14 @@ int main() {
 	
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < m; j++) {
-			scanf("%1s", &map[i][j]);
+			scanf("%1s", &arr[i][j]);
 			
-			if(map[i][j] == '*') {
+			if(arr[i][j] == '*') {
 				que.push({{i, j}, 0});
 				d[i][j] = 1;
 			}
 			
-			else if(map[i][j] == 'S') {
+			else if(arr[i][j] == 'S') {
 				x = i;
 				y = j;
 			}
