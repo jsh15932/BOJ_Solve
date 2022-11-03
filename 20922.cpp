739535a771ee92723f9ff72c1c333d100132f2db
jsh15932 <jsh1400512@naver.com>
2022-11-03 22:08:20 +0900

    Solve 20922

---
+++ 20922.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k, ans;
+int arr[100001];
+deque<int> dq;
+
+int main() {
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++) {
+        int num;
+
+        cin >> num;
+
+        arr[num]++;
+
+        if(dq.empty()) {
+            dq.push_back(num);
+        }
+
+        else {
+            if(arr[num] <= k) {
+                dq.push_back(num);
+            }
+
+            else {
+                int p = -1;
+
+                while(!dq.empty()) {
+                    p = dq.front();
+                    dq.pop_front();
+                    arr[p] -= 1;
+                }
+                
+                dq.push_back(num);
+            }
+        }
+
+        int size = dq.size();
+        ans = max(ans, size);
+    }
+
+    cout << ans;
+}
